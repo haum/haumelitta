8e7760297c9943fb5f78fcffcb9f4595a94b9132
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-05-17 15:36:21 +0200

    Separate sighandlers from core & remember last tweet

--- coffeesource.py
+++ coffeesource.py
@@ -39,6 +39,7 @@ import twitter
 import time
 import signal
 
+from sig_handlers import *
 from settings import *
 RE_START = re.compile(RE_START)
 RE_STOP = re.compile(RE_STOP)
@@ -50,54 +51,31 @@ def setup_gpio():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
 
-def SIGINT_handler(sig, stack):
-    """
-    Handler for SIGINT
-    Ensure a cleanup before exiting the program
-    """
 
-    print('Cleaning up')
-    GPIO.cleanup()
-    print('Quit...')
-    sys.exit()
-
-
-def SIGUSR_handler(sig, stack):
-    """
-    Handler for SIGUSR1 and SIGUSR2
-
-    SIGUSR1 => switch on coffee pot
-    SIGUSR2 => switch off coffee pot
-    """
-
-    if sig==signal.SIGUSR1:
-        GPIO.output(17, GPIO.HIGH)
-        print('Forced state : CoffeePot ON')
-    elif sig==signal.SIGUSR2:
-        GPIO.output(17, GPIO.LOW)
-        print('Forced state : CoffeePot OFF')
-
-
-def do_coffee(api):
+def do_coffee(api, last_id):
     """Must i send a signal to the coffee pot ?"""
     mention = api.GetMentions()[:1]
 
-    # check if twitto is one of the masters
-    master_detected = False
-    name = mention[0].user._screen_name
 
-    if name in MASTERS:
-        if RE_START.search(mention[0].text):
-            print("Hey ! Let's make coffee !")
-            GPIO.output(17, GPIO.HIGH)
-            return 0
-        elif RE_STOP.search(mention[0].text):
-            print("Yeah ! Coffee's ready !")
-            GPIO.output(17, GPIO.LOW)
-            return 0
+    if mention[0].id != last_id:
+
+        last_id  = mention[0].id
+        name = mention[0].user._screen_name
+
+        if name in MASTERS:
+
+            if RE_START.search(mention[0].text):
+                print("Hey ! Let's make coffee !")
+                GPIO.output(17, GPIO.HIGH)
+                return last_id
+
+            elif RE_STOP.search(mention[0].text):
+                print("Yeah ! Coffee's ready !")
+                GPIO.output(17, GPIO.LOW)
+                return last_id
     else:
         print("Waiting for a tweet...")
-        return 0
+        return last_id
 
 
 def main():
@@ -105,6 +83,7 @@ def main():
     signal.signal(signal.SIGINT, SIGINT_handler)
     signal.signal(signal.SIGUSR1, SIGUSR_handler)
     signal.signal(signal.SIGUSR2, SIGUSR_handler)
+
     # let's create an instance of the api
     try:
         api = twitter.Api(consumer_key=CONSUMER_KEY,\
@@ -145,8 +124,9 @@ CTRL-C or $ sudo kill -2 {0}""".format(os.getpid()))
 
 
     print("Entering main loop....")
+    last_id = 0
     while 1:
-        do_coffee(api)
+        last_id = do_coffee(api, last_id)
         time.sleep(UPDATE_TIME)
     return 0
 
---
+++ sig_handlers.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+# -*- coding:utf8 -*-
+#
+# sig_handlers.py
+#
+# Copyright © 2013 Mathieu Gaborit (matael) <mathieu@matael.org>
+#
+#
+# Distributed under WTFPL terms
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+#
+
+"""
+Signal handlers.
+
+SIGINT_handler : Graceful stop
+SIGUSR_handler : Forced states
+"""
+
+import signal
+
+def SIGINT_handler(sig, stack):
+    """
+    Handler for SIGINT
+    Ensure a cleanup before exiting the program
+    """
+
+    print('Cleaning up')
+    GPIO.cleanup()
+    print('Quit...')
+    sys.exit()
+
+
+def SIGUSR_handler(sig, stack):
+    """
+    Handler for SIGUSR1 and SIGUSR2
+
+    SIGUSR1 => switch on coffee pot
+    SIGUSR2 => switch off coffee pot
+    """
+
+    if sig==signal.SIGUSR1:
+        GPIO.output(17, GPIO.HIGH)
+        print('Forced state : CoffeePot ON')
+    elif sig==signal.SIGUSR2:
+        GPIO.output(17, GPIO.LOW)
+        print('Forced state : CoffeePot OFF')
+
+
+
+
