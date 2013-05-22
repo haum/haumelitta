f5de610585828540efc17c3ce71e0a2bfff69e5c
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-05-23 00:52:19 +0200

    main script

--- threaded/haumelitta.py
+++ threaded/haumelitta.py
@@ -27,5 +27,31 @@
 Threaded version of HAUMelitta's control script.
 """
 
+import signal
 
+import quick2wire.gpio as gpio
+
+from settings import *
+from globals import *
+from twitter_handler import TwitterHandler
+from sig_handlers import *
+
+
+def main():
+    """ main program """
+
+    # Open GPIO pin
+    with LK_commutable:
+        coffee_pin = gpio.pins.pin(PIN, direction=gpio.Out)
+        coffee_pin.open()
+
+    # setup interrupt handler for SIGINT
+    # setup forced state signals
+    signal.signal(signal.SIGINT, SIGINT_handler)
+    signal.signal(signal.SIGUSR1, SIGUSR_handler)
+    signal.signal(signal.SIGUSR2, SIGUSR_handler)
+
+    # init. twitter thread and start it
+    twitter_thread = TwitterHandler()
+    twitter_thread.start()
 
