d850d94f3f97b3e2ce89d1ef84b1327f5486374a
Jerome B <jerome@jblb.no-ip.com>
2013-06-01 20:10:09 +0200

    esssais de try: import

--- configure.py
+++ configure.py
@@ -9,30 +9,31 @@ import os
 
 
 try:
-    from setting import *
-    except IOError:
-"""
-        le reste fonction creation setting
-# twitter
-    TOKEN_KEY
-    TOKEN_SECRET
-    CONSUMER_KEY
-    CONSUMER_SECRET
-# GPIO
-    COFFEE_PIN
-    I2C_ADDR
-    I2C_INT
- """
+   	from setting import *
+except IOError:
+	"""
+    	le reste fonction creation setting
+		
+		# twitter
+    	TOKEN_KEY
+    	TOKEN_SECRET
+    	CONSUMER_KEY
+    	CONSUMER_SECRET
+		# GPIO
+    	COFFEE_PIN
+    	I2C_ADDR
+    	I2C_INT
+	"""
 # delete any setting file   
-    if os.path.exists("./setting.py"):
-        os.remove("./setting.py")
+if os.path.exists("./setting.py"):
+	os.remove("./setting.py")
 
 # open file for write
-   f = open("./setting.py",w)
-   f.write('#! /usr/bin/env python')
-   f.write('# -*- coding:utf8 -*-')
-   f.write('#')
-   f.write('TOKEN_KEY = ',input('twitter TOKEN_KEY: ')  )
+f = open("./setting.py",w)
+f.write('#! /usr/bin/env python')
+f.write('# -*- coding:utf8 -*-')
+f.write('#')
+f.write('TOKEN_KEY = ',input('twitter TOKEN_KEY: ')  )
 
 
 
@@ -40,3 +41,6 @@ try:
 # add TOKEN_KEY to file
 # ask TOKEN_SECRET
 # add TOKEN_SECRET to file
+
+
+f.close()
